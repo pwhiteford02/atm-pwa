11a5e2e96fb36558a2dea64eae7ab6996c93af6a
parker <pwhiteford02@gmail.com>
2019-04-02 10:08:38 -0400

    fixed app.js

--- app.js
+++ app.js
@@ -1,45 +1,52 @@
-
 "use strict";
 
 const DATA_HANDLER = require('./node/DataHandler');
+const FORMIDABLE = require('formidable');
 
 class app {
     constructor() {
+        this.data_handler = new DATA_HANDLER();
         this.ejsData = null;
-        this.fileName = `index.ejs`;
+        this.fileName = `index.html`;
         this.loadServer();
     }
 
     loadServer() {
+        const HTTPS = require('https');
+        // const HTTP = require('http');
         const EJS = require('ejs');
-        // const HTTPS = require('https');
-        const HTTP = require('http');
-        // const PORT = process.env.PORT || 443;
-        const PORT = process.env.PORT || 8111;
+        const PORT = process.env.PORT || 80;
         const SSL_OPTIONS = {
-            key: DATA_HANDLER.getKey(),
-            cert: DATA_HANDLER.getCert(),
+            key: this.data_handler.getKey(),
+            cert: this.data_handler.getCert(),
             requestCert: true,
             rejectUnauthorized: false
         };
 
-        // HTTPS.createServer(SSL_OPTIONS, async (request, response) => {
-        HTTP.createServer(async (request, response) => {
+        HTTPS.createServer(SSL_OPTIONS, async (request, response) => {
+            // HTTP.createServer(async (request, response) => {
 
             let httpHandler = (error, string, contentType) => {
-                if (error) {
+                /*if (request.headers['x-forwarded-proto'] !== 'https') {
+                    // response.redirect(`https://${request.header('host')}${request.url}`);
+                    response.writeHead(301, {Location: `https://${request.headers['host']}${request.url}`});
+                    response.end();
+                } else */if (error) {
                     response.writeHead(500, {'Content-Type': 'text/plain'});
                     response.end('An error has occurred: ' + error.message);
                 } else if (contentType.indexOf('css') >= 0 || contentType.indexOf('js') >= 0) {
+                    response.setHeader('Cache-Control', 'max-age=86400');
                     response.writeHead(200, {'Content-Type': contentType});
                     response.end(string, 'utf-8');
                 } else if (contentType.indexOf('html') >= 0) {
+                    response.setHeader('Cache-Control', 'max-age=86400');
                     response.writeHead(200, {'Content-Type': contentType});
                     response.end(EJS.render(string, {
                         data: this.ejsData,
                         filename: this.fileName
                     }));
                 } else {
+                    response.setHeader('Cache-Control', 'max-age=86400');
                     response.writeHead(200, {'Content-Type': contentType});
                     response.end(string, 'binary');
                 }
@@ -47,7 +54,42 @@ class app {
 
             if (request.method === 'POST') {
                 if (request.headers['x-requested-with'] === 'fetch.0') {
-                    return await DATA_HANDLER.receiveFile(request, response);
+                    let formData = {};
+                    new FORMIDABLE.IncomingForm().parse(request).on('field', (field, name) => {
+                        formData[field] = name;
+                    }).on('error', (err) => {
+                        next(err);
+                    }).on('end', () => {
+                        this.data_handler.insertRow(formData);
+                    });
+                } else if (request.headers['x-requested-with'] === 'fetch.1') {
+                    this.data_handler.getAllItems(function (fetchedData) {
+                        response.setHeader('Cache-Control', 'max-age=86400');
+                        response.writeHead(200, {'content-type': 'text/plain'});
+                        response.end(JSON.stringify(fetchedData));
+                    });
+                } else if (request.headers['x-requested-with'] === 'fetch.2') {
+                    let body = [];
+                    request.on('data', (chunk) => {
+                        body.push(chunk);
+                    }).on('end', () => {
+                        body = Buffer.concat(body).toString();
+                        this.data_handler.deleteItems(body);
+                    });
+                } else if (request.headers['x-requested-with'] === 'fetch.3') {
+                    let formData = {};
+                    new FORMIDABLE.IncomingForm().parse(request).on('field', (field, name) => {
+                        formData[field] = name;
+                    }).on('error', (err) => {
+                        next(err);
+                    }).on('end', () => {
+                        this.data_handler.queryData(formData, function(fetchedData) {
+                            response.setHeader('Cache-Control', 'max-age=86400');
+                            response.writeHead(200, {'content-type': 'text/plain'});
+                            console.log(JSON.stringify(fetchedData));
+                            response.end(JSON.stringify(fetchedData));
+                        });
+                    });
                 } else {
                     console.log(`Yo, somethings super wrong BDH!`);
                 }
@@ -57,22 +99,14 @@ class app {
                 DATA_HANDLER.renderDom(request.url.slice(1), 'application/javascript', httpHandler, 'utf-8');
             } else if (request.url.indexOf('.png') >= 0) {
                 DATA_HANDLER.renderDom(request.url.slice(1), 'image/png', httpHandler, 'binary');
-            } else if (request.url.indexOf('.woff') >= 0) {
-                DATA_HANDLER.renderDom(request.url.slice(1), 'application/font-woff', httpHandler, 'binary');
-            } else if (request.url.indexOf('.woff2') >= 0) {
-                DATA_HANDLER.renderDom(request.url.slice(1), 'application/font-woff2', httpHandler, 'binary');
-            } else if (request.url.indexOf('.ttf') >= 0) {
-                DATA_HANDLER.renderDom(request.url.slice(1), 'application/x-font-truetype', httpHandler, 'binary');
-            } else if (request.url.indexOf('.svg') >= 0) {
-                DATA_HANDLER.renderDom(request.url.slice(1), 'image/svg+xml', httpHandler, 'binary');
-            } else if (request.url.indexOf('.eot') >= 0) {
-                DATA_HANDLER.renderDom(request.url.slice(1), 'application/vnd.ms-fontobject', httpHandler, 'binary');
             } else if (request.url.indexOf('.ico') >= 0) {
                 DATA_HANDLER.renderDom(request.url.slice(1), 'image/x-icon', httpHandler, 'binary');
-            } else if (request.url.indexOf('results.ejs') >= 0) {
-                DATA_HANDLER.renderDom('public/views/results.ejs', 'text/html', httpHandler, 'utf-8');
+            } else if (request.url.indexOf('day_results.ejs') >= 0) {
+                DATA_HANDLER.renderDom('public/views/day_results.ejs', 'text/html', httpHandler, 'utf-8');
+            } else if (request.url.indexOf('night_results.ejs') >= 0) {
+                DATA_HANDLER.renderDom('public/views/night_results.ejs', 'text/html', httpHandler, 'utf-8');
             } else if (request.url.indexOf('/') >= 0) {
-                DATA_HANDLER.renderDom('public/views/index.ejs', 'text/html', httpHandler, 'utf-8');
+                DATA_HANDLER.renderDom('public/views/index.html', 'text/html', httpHandler, 'utf-8');
             } else {
                 DATA_HANDLER.renderDom(`HEY! What you're looking for: It's not here!`, 'text/html', httpHandler, 'utf-8');
             }
